c209ea33f2495bb4de6150560a3610022c4f3a62
Abdollah Zamannoun <abbezz@live.com>
2022-03-27 23:35:52 +0100

    Added Questions for Engineer and Intern

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
+const Manager = require("Manager");
+const Engineer = require("Engineer");
+const Intern = require("Intern");
 const fs = require("fs");
 const inquirer = require('inquirer');
 inquirer
 
 
-// start to write prompts for questions
-  .prompt([
+// start to write prompts for Manager questions
+
+   .prompt([   
     /* Pass your questions in here */
     {
       type: "rawlist",
       name: "options",
-      message: "Hi and welcome to Team profile generator. please select a option below.",
+      message: "Hi and welcome to Team profile generator. please select a option below 😀.", 
       choices: ["Add Manager", "Add Engineer", "Add Intern"],
 
     },
@@ -17,10 +21,10 @@ inquirer
     {
       type: "input",
       name: "name",
-      message: "What is your employees name?",
+      message: "What is your Manager name?",
       validate: (answer) => {
         if(answer --- ""){
-          return "Please enter a valid name"
+          return "Please enter a valid name 😡"
         }
         return true
       }
@@ -29,7 +33,7 @@ inquirer
     {
        type: "input",
        name: "id",
-       message: "Please enter your employees ID",
+       message: "Please enter your Manager ID",
        validate: (answer) => {
         if(isNaN(answer)){
           return "Please enter a valid ID number"
@@ -41,7 +45,7 @@ inquirer
     {
       type: "input",
       name: "Email",
-      message: "Enter your employees email please",
+      message: "Please enter your Manager email 📪",
       validate: (answer) => {
         if(answer --- ""){
           return "Please enter a valid name"
@@ -52,9 +56,118 @@ inquirer
 
     {
       type: "input",
+      name: "Number",
+      message: " Please Enter your Managers number",
+      validate: (answer) => {
+        if(isNaN(answer)){
+          return "Please enter a valid number"
+        }
+        return true
+      }
 
 
     },
+    
+    // start to write prompts for Engineer questions
+
+     {
+      type: "input",
+      name: "name",
+      message: "What is your Engineer name?",
+      validate: (answer) => {
+        if(answer --- ""){
+          return "Please enter a valid name 😡"
+        }
+        return true
+      }
+    },
+
+    {
+       type: "input",
+       name: "id",
+       message: "Please enter your Engineer ID",
+       validate: (answer) => {
+        if(isNaN(answer)){
+          return "Please enter a valid ID number"
+        }
+        return true
+      }
+    },
+
+    {
+      type: "input",
+      name: "Email",
+      message: "Please enter your email 📪",
+      validate: (answer) => {
+        if(answer --- ""){
+          return "Please enter a valid name"
+        }
+        return true
+      }
+    },
+
+    {
+      type: "input",
+      name: "gitName",
+      message: " Please enter your GitHub userName",
+      validate: (answer) => {
+        if(answer --- ""){
+          return "Please enter a valid GitHub userName"
+        }
+        return true
+      }
+
+
+    },
+
+    // start to write prompts for Intern questions
+
+    {
+      type: "input",
+      name: "name",
+      message: " Please enter the intern's name.",
+      validate: (answer) => {
+        if(answer --- ""){
+          return "Please enter a valid name 😡"
+        }
+        return true
+      }
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "Please enter intern's ID.",
+      validate: (answer) => {
+        if(isNaN(answer)){
+          return "Please enter a valid ID number"
+        }
+        return true
+      }
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Please enter intern's email.",
+      validate: (answer) => {
+        if(answer --- ""){
+          return "Please enter a valid email 😡"
+        }
+        return true
+      }
+    },
+    {
+      type: "input",
+      name: "school",
+      message: "Please enter intern's school.",
+      validate: (answer) => {
+        if(answer --- ""){
+          return "Please enter a valid school name 😡"
+        }
+        return true
+      }
+    },
+
+
 
   ])
   .then((answers) => {
