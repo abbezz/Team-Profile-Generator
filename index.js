0e1852c10e725854930254162323c456bade9d3b
Abdollah Zamannoun <abbezz@live.com>
2022-03-27 22:52:38 +0100

    Created Index.js to my Team-Profile-Generator

--- index.js
+++ index.js
@@ -1,185 +1,70 @@
-//fs for writing file
-const fs = require('fs');
-
-//inquirer for prompting user for their response
+const fs = require("fs");
 const inquirer = require('inquirer');
-
-// importing constructor functions
-const Employee = require('./lib/Employee');
-const Engineer = require('./lib/Engineer');
-const Intern = require('./lib/Intern');
-const Manager = require('./lib/Manager');
-
-//importing layout template file
-const generateTemplate = require('./src/layout-template');
+inquirer
 
 
-//importing generate function 
-const generateHtml = require('./generate-html');
+// start to write prompts for questions
+  .prompt([
+    /* Pass your questions in here */
+    {
+      type: "rawlist",
+      name: "options",
+      message: "Hi and welcome to Team profile generator. please select a option below.",
+      choices: ["Add Manager", "Add Engineer", "Add Intern"],
 
-//creating empty array to store user input
+    },
 
-const teamMembers = []
+    {
+      type: "input",
+      name: "name",
+      message: "What is your employees name?",
+      validate: (answer) => {
+        if(answer --- ""){
+          return "Please enter a valid name"
+        }
+        return true
+      }
+    },
 
-//prompting userinput 
+    {
+       type: "input",
+       name: "id",
+       message: "Please enter your employees ID",
+       validate: (answer) => {
+        if(isNaN(answer)){
+          return "Please enter a valid ID number"
+        }
+        return true
+      }
+    },
 
-function userInfo() {
-    inquirer.prompt([
-        {
-            type: 'input',
-            message: 'Enter Employee Name: ',
-            name: 'name',
-            validate: function (nameInput) {
-                if (nameInput) {
-                    return true;
-                } else {
-                    return 'Please enter Employee Name.';
-                }
-            }
-        },
+    {
+      type: "input",
+      name: "Email",
+      message: "Enter your employees email please",
+      validate: (answer) => {
+        if(answer --- ""){
+          return "Please enter a valid name"
+        }
+        return true
+      }
+    },
 
-        {
-            type: 'input',
-            message: 'Enter Email: ',
-            name: 'email',
-            validate: function (emailInput) {
-                if (emailInput) {
-                    return true;
-                } else {
-                    return 'Please enter Employee e-mail address.';
-                }
-            }
-        },
-
-        {
-            type: 'input',
-            message: '235: ',
-            name: 'id',
-            validate: function (idInput) {
-                if (idInput) {
-                    return true;
-                } else {
-                    return 'Please enter Employee Id.';
-                }
-            }
-        },
-
-
-
-        {
-            type: 'list',
-            message: 'Select Role: ',
-            name: 'role',
-            choices: ["Manager", "Engineer", "Intern"]
-
-        },
-
-    ])
-    .then(answers => {
-
-            if (answers.role === 'Manager') {
-                inquirer.prompt([
-                    {
-                        type: 'input',
-                        name: 'office',
-                        message:'Enter office number:',
-                        validate: officeInput => {
-                            if (officeInput) {
-                                return true;
-                            } else {
-                                return 'Please enter office No.';
-                            }
-                        }
-                        
-                    }
-                ])
-                .then(response => {
-                    console.log(response.office);
-                    const ManagerTeam = new Manager (answers.name, answers.email, answers.id, answers.role, response.office)
-                    teamMembers.push(ManagerTeam);
-                    addOption()
-                })
-            }else if(answers.role === 'Engineer' ){
-                inquirer.prompt([
-                    {
-                        type: 'input',
-                        name: 'gitHub',
-                        message:'Enter Github name:',
-                        validate: githubInput => {
-                            if (githubInput) {
-                                return true;
-                            } else {
-                                return 'Please enter gitHub username.';
-                            }
-                        }
-                    }
-                ])
-                .then(response => {
-                    console.log(response.gitHub);
-                    const EngineerTeam = new Engineer (answers.name, answers.email, answers.id, answers.role, response.gitHub)
-                    teamMembers.push(EngineerTeam);
-                    addOption()
-                })
-            } else if (answers.role === 'Intern'){
-                inquirer.prompt([
-                    {
-                        type: 'input',
-                        name: 'school',
-                        message:'Enter School name:', 
-                        validate: function (schoolInput) {
-                            if (schoolInput) {
-                                return true;
-                            } else {
-                                return 'Please enter school  name.';
-                            }
-                        }
-
-                    }
-                ])
-                .then(response =>{
-                    console.log(response.school);
-                    const internTeam = new Intern (answers.name,  answers.email, answers.id, answers.role, response.school)
-                    teamMembers.push(internTeam);
-                    addOption()
-                })
-            }
-
-            else {
-                const employeeTeam = new Employee (answers.name, answers.email, answers.id, answers.role);
-                teamMembers.push(employeeTeam);
-                addOption()
-
-            }
-
-            //add option 
-
-            function addOption() {
-                inquirer.prompt([
-                    {
-                        type:'confirm',
-                        name: 'addMore',
-                        message:'Would you like to add another Employee?'
-                    }
-                ])
-                .then(res =>{
-                    if(res.addMore === true){
-                        userInfo(teamMembers);
-                    }else{
-                        console.log('team', teamMembers)
-                        let cardLayoutHtml = generateTemplate(teamMembers);
-                        generateHtml(cardLayoutHtml)
-                    }
-                })
-            }
-        })
-}
+    {
+      type: "input",
 
 
-userInfo();
+    },
 
-
-
-
-
-
-
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+    console.log(answers);
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
